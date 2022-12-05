71c0096d7d0e4c25e65137f1e2871be350b0d659
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-12-05 22:17:47 +0300

    added background video to home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import home from '../home/home.css'
-
-function Home() {
+import './Home.css'
+const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <video autoPlay muted loop>
+          <source src='https://res.cloudinary.com/dnhtposui/video/upload/v1670223314/pexels-c-technical-6334253_wiwy4l.mp4'/>
+        </video>
+        <div className='home_text'>
+          <h1>
+            {/* Welcome to moviefy */}
+          </h1>
+          <p>
+            This here, you get to know the latest movies, and after watching them, you can come and create your own personal favourites list by liking a movie 5+ times(similar to giving a movie a 5-star rating).
+            Happy watching and Enjoy moviefy!!
+          </p>
+        </div>
+    </div>
   )
 }
-
 export default Home
