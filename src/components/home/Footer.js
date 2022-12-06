465510e4f1873e099682ce58721a4c85b8892bac
joymush <joy.chemutai@student.moringaschool.com>
2022-12-06 08:02:02 +0300

    add contact, and social handles

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaPhoneSquareAlt, FaRegEnvelope, FaInstagram, FaFacebookSquare, FaTwitterSquare } from "react-icons/fa";
 import "./Home"
 
 function Footer () {
@@ -6,6 +7,18 @@ function Footer () {
         <div className="footer">
             <div className="section1">
                 <h2>Contacts</h2>
+                <div className="footer-contact">
+                    <p><FaPhoneSquareAlt/>0714244241</p>
+                    <FaRegEnvelope />
+                    <p>info@bookreview</p>
+                </div>
+            </div>
+
+            <div className="section2">
+                <h2>Socials</h2>
+                <div className="socials">
+                    <FaInstagram /> <FaFacebookSquare /> <FaTwitterSquare />
+                </div>
             </div>
         </div>
     )
