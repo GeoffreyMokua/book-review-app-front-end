1cf7e7a28cb5bbabd0f533f25aca5195baaaf27a
joymush <joy.chemutai@student.moringaschool.com>
2022-12-06 07:35:07 +0300

    add changes to the Navbar

---
+++ src/components/home/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home"
+
+function Footer () {
+    return (
+        <div className="footer">
+            <div className="section1">
+                <h2>Contacts</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './Home.css'
+import './home.css'
+
 const Home = () => {
   return (
     <div className='home'>
