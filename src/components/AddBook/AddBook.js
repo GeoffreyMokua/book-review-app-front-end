07ad62d86ead37bba71032a8cc5bbcc0b0839ea5
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-12-07 10:37:15 +0300

    added background images to login and addbook components

--- src/components/AddBook/AddBook.js
+++ src/components/AddBook/AddBook.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './AddBook.css'
 
-const AddBook = () => {
+export default function AddBook() {
   return (
-    <div>AddBook</div>
+    <div className="parent-container">
+    <div className="image-background">
+      <img src="https://res.cloudinary.com/dnhtposui/image/upload/v1670398207/debby-hudson-ERb-JXVwAfo-unsplash_1_ws2ira.jpg" alt="Background Image" />
+    </div>
+    </div>
   )
 }
-
-export default AddBook
