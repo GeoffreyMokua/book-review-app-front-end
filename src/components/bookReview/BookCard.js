cffd9e1469bc23257c27f5dedb3ba90e6f9c34c9
joymush <joy.chemutai@student.moringaschool.com>
2022-12-09 11:03:20 +0300

    fetch books api

--- src/components/bookReview/BookCard.js
+++ src/components/bookReview/BookCard.js
@@ -1,9 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+// import { Link } from "react-router-dom";
+// import axios from "axios";
+import './BookCard.css';
 
-function BookCard() {
+const BookList = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch('/books')
+      .then(r => r.json())
+      .then(books => {
+        console.log(books)
+        setBooks(books)
+      })
+  }, [])
+
   return (
-    <div>Book List</div>
-  )
-}
+    <div>
+      <h1>Book List</h1>
+      {books.map(book => (
+        <div>
+          <h4>{book.title}</h4>
 
-export default BookCard
+          <h4>Author: {book.author}</h4>
+          <p>Description: {book.description}</p>
+          {book.reviews.map(review => (
+            <>
+              <p>Rating: {review.rating}</p>
+              <p>Review: {review.comment}</p>
+            </>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+// const Book = ({ book }) => (
+//   <div>
+//     <h2>
+//       <Link to={`/books/${book.id}`}>{book.title}</Link>
+//     </h2>
+//     <p>By {book.author}</p>
+//   </div>
+// );
+
+// const BookDetail = ({ match }) => {
+//   const [book, setBook] = useState(null);
+//   const [rating, setRating] = useState(0);
+//   const [comment, setComment] = useState("");
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+
+//   useEffect(() => {
+      // axios
+//       .get(`/api/books/${match.params.id}`)
+//       .then(response => {
+//         setBook(response.data);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   }, [match.params.id]); 
+  
+//   const handleRatingChange = event => {
+//     setRating(Number(event.target.value));
+//   };
+
+//   const handleCommentChange = event => {
+//     setComment(event.target.value);
+//   };
+
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     setIsSubmitting(true);
+//     axios
+//       .post(`/api/books/${match.params.id}/comments`, {
+//         rating,
+//         comment
+//       })
+//       .then(response => {
+//         setBook(response.data);
+//         setRating(0);
+//         setComment("");
+//         setIsSubmitting(false);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         setIsSubmitting(false);
+//       });
+//   };
+
+//   if (!book) {
+//     return null;
+//   }
+  
+//   return (
+//     <div>
+//       <h1>{book.title}</h1>
+//       <p>By {book.author}</p>
+//       <h2>Ratings and Comments</h2>
+//       <h3>Leave a Comment</h3>
+//       <form onSubmit={handleSubmit}>
+
+//       </form>
+//     </div>
+ export default BookList;
+
